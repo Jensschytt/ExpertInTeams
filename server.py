bf97108f5cae89c764b2fdd229eea0588167c31a
tsoer18 <tsoer18@student.sdu>
2024-09-19 12:51:58 +0200

    vis tracking and removed duplicate

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ import pickle
 import socket
 import struct
 import cv2
+from ultralytics import YOLO
+
+# Load the YOLOv8 model (e.g., yolov8n for speed)
+model = YOLO('yolov8n.pt')
 
 HOST = ''
 PORT = 8089
@@ -22,14 +26,7 @@ payload_size = struct.calcsize("L") ### CHANGED
 
 
 
-
-
-
-
 while True:
-
-
-
 
     # Retrieve message size
     while len(data) < payload_size:
@@ -49,6 +46,27 @@ while True:
     # Extract frame
     frame = pickle.loads(frame_data)
 
+
+
+    # Run YOLOv8 inference on the frame
+    results = model(frame, verbose=False)
+
+    # Get detected labels from the result
+    for result in results:
+        labels = result.names  # Accessing label names
+        detected_classes = result.boxes.cls
+
+    # Convert class indexes to labels and print
+        for class_index in detected_classes:
+            print(f"Detected object: {labels[int(class_index)]}")
+    
+
+    print("test")
+    # Visualize the results (bounding boxes, labels, etc.)
+    annotated_frame = results[0].plot()
+
+    # Display the frame with detections
+    cv2.imshow("YOLOv8 Real-time Detection", annotated_frame)
+
     # Display
-    cv2.imshow('frame', frame)
     cv2.waitKey(1)
